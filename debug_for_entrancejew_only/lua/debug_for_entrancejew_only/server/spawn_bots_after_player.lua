a1dade982ec9c656d1a47f47213a49ea67005896
EntranceJew <EntranceJew@gmail.com>
2023-09-15 22:03:28 -0500

    SMORE: bot options, friendly fire

--- debug_for_entrancejew_only/lua/debug_for_entrancejew_only/server/spawn_bots_after_player.lua
+++
@@ -1,9 +0,0 @@
-hook.Add( "PlayerInitialSpawn", "ej_debug_spawn_bots_after_player", function()
-    hook.Remove( "PlayerInitialSpawn", "ej_debug_spawn_bots_after_player" )
-    local delay = 0.4
-    for i = player.GetCount() + 1, game.MaxPlayers() do
-        timer.Simple(i * delay, function()
-            RunConsoleCommand("ttt_bot_add")
-        end)
-    end
-end )
